f333daab1ed9f75b3263c427ef17718b4665b068
centiger <centiger2@gmail.com>
2026-05-23 20:23:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cen-matrix-v26-repeated-nation-labels';
+const CACHE_NAME='cen-matrix-v27-multi-select';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients.claim();});
 self.addEventListener('fetch',e=>{});
