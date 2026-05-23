be3a46b2637b8402a96c88cb4bcfa97c5f8eb5ab
centiger <centiger2@gmail.com>
2026-05-24 05:08:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cen-matrix-v34-king-timeline-labels';
+const CACHE_NAME='cen-matrix-v35-king-line-nodes';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients.claim();});
 self.addEventListener('fetch',e=>{});
