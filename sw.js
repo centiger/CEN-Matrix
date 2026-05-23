8319edc7d148592b6dee2a919142690a5ff8f453
centiger <centiger2@gmail.com>
2026-05-24 08:46:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cen-matrix-kings-restored-v1';
+const CACHE_NAME='cen-matrix-real-proportional-kings-v1';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event => {
