444af09e46e60ccdfa68eb26e8d5645322f9b463
centiger <centiger2@gmail.com>
2026-05-24 05:39:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cen-matrix-v35-king-line-nodes';
+const CACHE_NAME='cen-matrix-v36-clean-king-bars';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients.claim();});
 self.addEventListener('fetch',e=>{});
