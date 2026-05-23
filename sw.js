0dc2b95494aa4dd12835e4a94bf538efbddb8786
centiger <centiger2@gmail.com>
2026-05-23 20:33:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cen-matrix-v28-all-toggle';
+const CACHE_NAME='cen-matrix-v29-tyresidon-repeat';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients.claim();});
 self.addEventListener('fetch',e=>{});
