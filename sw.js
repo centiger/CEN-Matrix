ff43bdbe7e3f4e9bde727ef69d403156b5f60d31
centiger <centiger2@gmail.com>
2026-05-23 21:05:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cen-matrix-v30-split-kings';
+const CACHE_NAME='cen-matrix-v31-accession-kings';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{self.clients.claim();});
 self.addEventListener('fetch',e=>{});
