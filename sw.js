1075c8a08707972497f9a723586cf8aec0ab7595
centiger <centiger2@gmail.com>
2026-05-23 15:40:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cen-matrix-v0-1';
+const CACHE_NAME = 'cen-matrix-v3-1';
 const ASSETS = ['./','./index.html','./manifest.json','./sw.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
